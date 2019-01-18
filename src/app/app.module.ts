dda97384b68c000ca516da11198deff3503246cd
drewheathens <mudavadie@gmail.com>
2019-01-18 17:27:43 +0300

    added classes user and repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    UserComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule

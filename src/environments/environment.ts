712c08c46844cbe0bbd4a40a02e680d413a1e56c
drewheathens <mudavadie@gmail.com>
2019-01-24 08:48:47 +0300

    changes on api url environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ import { appInitializerFactory } from "@angular/platform-browser/src/browser/ser
 
 export const environment = {
   production: false,
-  apiUrl:"ad3838bd6c7434992e27d4260a5e13e343ed3db4"
+  apiUrl:"2ea252f267dc34fb1385fab8c60c4bd01623a8e3"
 };
